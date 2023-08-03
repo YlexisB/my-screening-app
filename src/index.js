980537180d99660e7b97f66bc4ab9c7c1e8f5a10
Ylexis Butler <ylexislauren@gmail.com>
2023-08-03 01:51:35 -0400

    finished intyerview quesitionsweb app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import reportWebVitals from "./reportWebVitals";
+import Home from "./Home";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const App = () => {
+  return <Home />;
+};
+
 root.render(
   <React.StrictMode>
     <App />
